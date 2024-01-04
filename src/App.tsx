b81170961fdfe417e982ceb6e6ef6cf5355b3c81
minji2219 <200146@jnu.ac.kr>
2024-01-04 18:03:08 +0900

    route 처리
    react-router-dom 설치
    router을 이용하여 각 페이지로 라우터 되게 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/posts">post</Link>
+          </li>
+          <li>
+            <Link to="/posts/:id">post detail</Link>
+          </li>
+          <li>
+            <Link to="/posts/new">post new</Link>
+          </li>
+          <li>
+            <Link to="/posts/edit/:id">post edit</Link>
+          </li>
+          <li>
+            <Link to="/posts/profile">profile</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path="/posts" element={<h1>post</h1>} />
+        <Route path="/posts/:id" element={<h1>post detail</h1>} />
+        <Route path="/posts/new" element={<h1>post new</h1>} />
+        <Route path="/posts/edit/:id" element={<h1>post edit</h1>} />
+        <Route path="/profile" element={<h1>profile</h1>} />
+        {/* 위의 경로가 아닌 다른 경로로 들어올땐 메인 페이지로 replace해주기 */}
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
   );
 }
 
