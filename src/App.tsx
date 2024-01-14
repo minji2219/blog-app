e083a1668284a657ec85c8149ecc99b28367bf0e
minji2219 <200146@jnu.ac.kr>
2024-01-15 00:45:55 +0900

    context api로 dark모드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Router from "./components/Router";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Loader from "components/Loader";
+import ThemeContext from "context/ThemeContext";
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
   //auth를 체크하기 전에 loader를 띄워주는 용도
   const [init, setInit] = useState(false);
@@ -24,10 +26,10 @@ function App() {
     });
   }, []);
   return (
-    <>
+    <div className={context.theme === "light" ? "light" : "dark"}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   );
 }
 
